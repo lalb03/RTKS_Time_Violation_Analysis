557ebd72b13d75f9ca68decad0a3a7ec8c543b2e
Caterina Vallotto <caterina.vallotto@studenti.unipd.it>
2026-07-17 17:01:02 +0200

    run experiment

--- ArduPilot/ArduCopter/mode_auto.cpp
+++ ArduPilot/ArduCopter/mode_auto.cpp
@@ -3,8 +3,8 @@
 // [TimeTrap] define delay
 #include <unistd.h>
 #include <stdio.h>
-#define TT_DELAY 30000
-// #define TT_DELAY 50000
+// #define TT_DELAY 30000
+#define TT_DELAY 50000
 // -----
 
 #if MODE_AUTO_ENABLED == ENABLED
